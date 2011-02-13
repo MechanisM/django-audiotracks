7b4b44ebf79125eba7514a4416b77e1eca1c8a39
Alex Marandon <contact@alexmarandon.com>
2011-02-14 02:31:27 +0700

    Specify upload dir for tests
    and let git ignore it

--- tests/settings.py
+++ tests/settings.py
@@ -1,3 +1,8 @@
+import os
+
+HERE_DIR = os.path.dirname(os.path.abspath(__file__))
+MEDIA_ROOT = os.path.join(HERE_DIR, "site_media")
+
 DATABASE_ENGINE = 'sqlite3'
 ROOT_URLCONF = ''
 SITE_ID = 1
@@ -9,3 +14,4 @@ INSTALLED_APPS = (
     "audiotracks",
 
 )
+
