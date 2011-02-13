0cf6521fef822708c4357d826df3ac14e62631a5
Alex Marandon <contact@alexmarandon.com>
2011-02-14 02:20:49 +0700

    Add example project

---
+++ example_project/urls.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+     (r'^$', 'main.views.index'),
+    url("^(?P<username>[\w\._-]+)/music", include("audiotracks.urls")),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+     (r'^admin/', include(admin.site.urls)),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', 
+            {'document_root': settings.MEDIA_ROOT}),
+    )
