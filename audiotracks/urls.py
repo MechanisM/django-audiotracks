dc32a770a75e1b3211cf76ad03da238955409ed6
Alex Marandon <contact@alexmarandon.com>
2011-04-24 12:43:54 +0700

    Accept final / for app top level URL

--- audiotracks/urls.py
+++ audiotracks/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns("audiotracks.views",
-    url("^$", "index", name="audiotracks"),
+    url("^/?$", "index", name="audiotracks"),
     url("^/track/(?P<track_slug>.*)$", "track_detail", name="track_detail"),
     url("^/upload", "upload_track", name="upload_track"),
     url("^/edit/(?P<track_id>.+)", "edit_track", name="edit_track"),
